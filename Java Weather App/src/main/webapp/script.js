5f6340f123ff0e6edcac7ab447630781dcf70871
Anthony Lapham <lapham.2@wright.edu>
2021-11-25 20:40:38 -0500

    Trying to make submit button and input values work.

--- Java Weather App/src/main/webapp/script.js
+++ Java Weather App/src/main/webapp/script.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
+  var button = document.querySelector('#submitButton');
+  var inputValue = document.querySelector('#inputValue');
   var apiID = config.API_ID;
-  var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Dayton&units=imperial&appid=" + apiID;
+  var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&units=imperial&appid=" + apiID;
 
-  $.getJSON(apiURL, function(data) {
+
+
+$('#submitButton').click(function(){
+	  $.getJSON(apiURL, function(data) {
       console.log(data);
       document.getElementById('weather').innerHTML = data.weather.description;
       //document.getElementById('geo').innerHTML = "latitude: " + data.current_observation.display_location.latitude + " longitude: " + data.current_observation.display_location.longitude;
-      document.getElementById('temp').innerHTML = data.main.temp;
+      document.getElementById('temp').innerHTML = data[main][0][temp];
       document.getElementById('city').innerHTML = data.name;
       document.getElementById('country').innerHTML = data.sys.country;
       document.getElementById('icon').src= data.weather.icon;
   });
+});
+
+	
 
 });
