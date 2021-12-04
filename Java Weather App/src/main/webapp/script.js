c10cae4f9bdc528c5dd23ce4da099d1c95fd1d21
Anthony Lapham <lapham.2@wright.edu>
2021-12-04 13:32:22 -0500

    Input now works

--- Java Weather App/src/main/webapp/script.js
+++ Java Weather App/src/main/webapp/script.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
-  var button = document.querySelector('#submitButton');
-  var inputValue = document.querySelector('#inputValue');
+  //var button = document.querySelector('#submitButton');
+ // var inputValue = document.querySelector('#inputValue').value;
   var apiID = config.API_ID;
-  var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&units=imperial&appid=" + apiID;
+  //var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial&appid=" + apiID;
 
 
 
 $('#submitButton').click(function(){
+	var inputValue = document.querySelector('#inputValue').value;
+	var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&units=imperial&appid=" + apiID;
 	  $.getJSON(apiURL, function(data) {
       console.log(data);
-      document.getElementById('weather').innerHTML = data.weather.description;
+      document.getElementById('weather').innerHTML = data['weather'][0]['description'];
       //document.getElementById('geo').innerHTML = "latitude: " + data.current_observation.display_location.latitude + " longitude: " + data.current_observation.display_location.longitude;
-      document.getElementById('temp').innerHTML = data[main][0][temp];
+      document.getElementById('temp').innerHTML = data.main.temp;
       document.getElementById('city').innerHTML = data.name;
       document.getElementById('country').innerHTML = data.sys.country;
       document.getElementById('icon').src= data.weather.icon;
